e18ec905b003f818210ad93a3a33d2ec2c34ea8e
Waleed-PC\Waleed <devwaleed@gmail.com>
2017-12-13 21:52:11 +0500

    Added available quantity column to Invoices

--- StockManager/App_Start/RouteConfig.cs
+++ StockManager/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace StockManager
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+               
             routes.MapRoute(
             name: "NoAccess",
             url: "NoAccess",
