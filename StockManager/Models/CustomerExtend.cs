2c740b3a8f8353a324ca577648351742a115782e
Ankur <Ankur@LAPTOP-K0DFQCFE>
2017-12-14 21:03:42 +0530

    Vendor and Customer Validation

---
+++ StockManager/Models/CustomerExtend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StockManager.Models
+{
+    [MetadataType(typeof(CustomerMetaData))]
+    public partial class Customer { }
+
+    class CustomerMetaData
+    {
+        
+        [Required(ErrorMessage = "This field is required")]
+        public string CustomerName { get; set; }
+
+    }
+
+}
---
+++ StockManager/Models/VendorExtend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StockManager.Models
+{
+    [MetadataType(typeof(VendorMetaData))]
+    public partial class Vendor { }
+
+    class VendorMetaData
+    {
+        [Required(ErrorMessage = "This field is required")]
+        public Nullable<int> VendorTypeId { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string VendorName { get; set; }
+        
+    }
+
+}
